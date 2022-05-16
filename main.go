74c44eb500fc8a1b306a123b951283a485a7d271
Vít Kotačka <vit.kotacka@oracle.com>
2022-05-16 16:31:18 +0200

    Implement downstream services

--- main.go
+++ main.go
@@ -1,26 +1,58 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
+	"flag"
+	"fmt"
+	"github.com/google/uuid"
 	"github.com/gorilla/mux"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"github.com/sirupsen/logrus"
 	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/propagation"
 	"log"
+	"math/rand"
 	"net/http"
+	"os"
+	"runtime"
+	"time"
 )
 
 const (
-	appName      = "my-app"
-	hdrRequestId = "X-Request-ID"
-	hdrTracingId = "X-Tracing-ID"
-	appAddr      = "0.0.0.0:4040"
-	logFile      = "_logs/observability.log"
-	tracingUrl   = "http://localhost:14268/api/traces"
+	defaultAppName = "my-app"
+	defaultAppPort = "4040"
+	hdrRequestId   = "X-Request-ID"
+	hdrTracingId   = "X-Tracing-ID"
+	hdrUserAgent   = "User-Agent"
+	logFile        = "_logs/observability.log"
+	logFilePattern = "_logs/%s.log"
+	tracingUrl     = "http://grafana.edu.dobias.info:14268/api/traces"
+)
+
+var (
+	appAddr       string
+	appName       string
+	appPort       string
+	downstreamUrl string
+	goVersion     = runtime.Version()
 )
 
 func init() {
+	// parse command line arguments
+	flag.StringVar(&appName, "n", defaultAppName, "Application name.")
+	flag.StringVar(&appPort, "p", defaultAppPort, "Application port.")
+	flag.StringVar(&downstreamUrl, "d", "", "Downstream URL. Empty string triggers no call to downstream service.")
+	printHelp := flag.Bool("h", false, "Print help.")
+	flag.Parse()
+	if *printHelp {
+		fmt.Fprintf(flag.CommandLine.Output(), "Options:\n")
+		flag.PrintDefaults()
+		os.Exit(0)
+	}
+	appAddr = fmt.Sprintf("0.0.0.0:%s", appPort)
+	// Set logging
 	logrus.SetFormatter(&logrus.TextFormatter{
 		FullTimestamp: true,
 	})
@@ -39,9 +71,8 @@ func init() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	// Register our TracerProvider as the global so any imported
-	// instrumentation in the future will default to using it.
 	otel.SetTracerProvider(tp)
+	otel.SetTextMapPropagator(propagation.TraceContext{})
 }
 
 func main() {
@@ -57,9 +88,13 @@ func main() {
 }
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
-	_, span := otel.Tracer(appName).Start(r.Context(), "homeHandler")
+	spanCtx, span := otel.Tracer(appName).Start(r.Context(), "homeHandler")
 	defer span.End()
 	log := requestLog("homeHandler", r)
+	if downstreamUrl != "" {
+		callDownstream(r, spanCtx, downstreamUrl)
+	}
+	randomizeLatency()
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	resp := make(map[string]string)
@@ -71,4 +106,44 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
 	log.Infof("writing response with status: %d", http.StatusOK)
 	w.Write(jsonResp)
 	return
+}
+
+func randomizeLatency() {
+	rand.Seed(time.Now().UnixNano())
+	n := rand.Intn(1000)
+	time.Sleep(time.Duration(n) * time.Millisecond)
+}
+
+func callDownstream(r *http.Request, spanCtx context.Context, url string) {
+	_, span := otel.Tracer(appName).Start(spanCtx, "callDownstream")
+	defer span.End()
+	log := requestLog("callDownstream", r)
+	log.Infof("calling downstream service: %s", url)
+	downstreamRequest := getDownstreamRequest(r, url)
+	spannedRequest := downstreamRequest.WithContext(spanCtx)
+	client := getClient()
+	resp, err := client.Do(spannedRequest)
+	if err != nil {
+		log.Errorf("error calling downstream service: %s", err)
+	}
+	log.Infof("downstream service returned http code: %d", resp.StatusCode)
+	log.Infof("downstream service returned request id: %s", resp.Header.Get(hdrRequestId))
+}
+
+func getDownstreamRequest(r *http.Request, url string) *http.Request {
+	downstreamRequest, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		log := requestLog("getDownstreamRequest", r)
+		log.Errorf("error assembling downstream request: %s", err)
+	}
+	downstreamRequest.Header.Set(hdrUserAgent, fmt.Sprintf("Golang/%s", goVersion))
+	downstreamRequest.Header.Add(hdrRequestId, uuid.New().String())
+	// TODO Vita: set tracing id
+	return downstreamRequest
+}
+
+func getClient() *http.Client {
+	return &http.Client{
+		Timeout: time.Second * 10,
+	}
 }
